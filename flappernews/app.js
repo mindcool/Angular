9579f8dafba45e03c44a4fb9198db21cf04fab41
Mehmet Noyan AYDIN <zerocoder@gmail.com>
2015-10-09 14:31:54 -0600

    Start writing backend with NodeJS

--- flappernews/app.js
+++ flappernews/app.js
@@ -8,6 +8,11 @@ app.config([
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
       });
       
       $urlRouterProvider.otherwise('home')
@@ -32,7 +37,12 @@ function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes:0},
+        {author: 'Bob', body: 'Great idea but everything is wrong', upvotes:0}
+      ],
+      
     });
     $scope.title = '';
     $scope.link = '';
@@ -42,3 +52,19 @@ function($scope, posts){
     post.upvotes += 1;
   };
 }]);
+app.controller('PostsCtrl', [
+  '$scope', 
+  '$stateParams', 
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+  }]);
